d1549a47555878ff89501689a3826dd10ba08e03
Tom Prince <mozilla@hocat.ca>
2018-11-30 15:53:12 -0700

    Add package skeleton.

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+with open("requirements/base.in", "r") as fp:
+    requirements = fp.read().splitlines()
+
+setup(
+    name="taskgraph",
+    version="0.0.1",
+    description="Build taskcluster taskgraphs",
+    url="https://hg.mozilla.org/ci/taskgraph",
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    install_requires=requirements,
+    classifiers=("Programming Language :: Python :: 2",),
+)
