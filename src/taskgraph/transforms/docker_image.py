041b78252b9e44dabf3ee32ab4c80bd1c0a64de5
Tom Prince <mozilla@hocat.ca>
2019-04-03 17:08:40 -0600

    Bug 1532783: Remove support for worker-types not using aliases; r=me

--- src/taskgraph/transforms/docker_image.py
+++ src/taskgraph/transforms/docker_image.py
@@ -161,8 +161,7 @@ def fill_template(config, tasks):
                 'tier': 1,
             },
             'run-on-projects': [],
-            'worker-type': 'aws-provisioner-v1/gecko-{}-images'.format(
-                config.params['level']),
+            'worker-type': 'images',
             'worker': {
                 'implementation': 'docker-worker',
                 'os': 'linux',
--- src/taskgraph/util/workertypes.py
+++ src/taskgraph/util/workertypes.py
@@ -7,26 +7,18 @@ from __future__ import absolute_import, print_function, unicode_literals
 from .keyed_by import evaluate_keyed_by
 from .memoize import memoize
 
-WORKER_TYPES = {
-    'aws-provisioner-v1/gecko-misc': ('docker-worker', 'linux'),
-}
-
 
 @memoize
 def worker_type_implementation(graph_config, worker_type):
     """Get the worker implementation and OS for the given workerType, where the
     OS represents the host system, not the target OS, in the case of
     cross-compiles."""
-    if '/' in worker_type:
-        worker_type = worker_type.replace('{level}', '1')
-        return WORKER_TYPES[worker_type]
-    else:
-        worker_config = evaluate_keyed_by(
-            {"by-worker-type": graph_config["workers"]["aliases"]},
-            "worker-types.yml",
-            {'worker-type': worker_type},
-        )
-        return worker_config['implementation'], worker_config['os']
+    worker_config = evaluate_keyed_by(
+        {"by-worker-type": graph_config["workers"]["aliases"]},
+        "worker-types.yml",
+        {'worker-type': worker_type},
+    )
+    return worker_config['implementation'], worker_config['os']
 
 
 @memoize
@@ -35,18 +27,14 @@ def get_worker_type(graph_config, worker_type, level):
     Get the worker type based, evaluating aliases from the graph config.
     """
     level = str(level)
-    if '/' in worker_type:
-        worker_type = worker_type.format(level=level)
-        return worker_type.split("/", 1)
-    else:
-        worker_config = evaluate_keyed_by(
-            {"by-worker-type": graph_config["workers"]["aliases"]},
-            "worker-types.yml",
-            {"worker-type": worker_type},
-        )
-        worker_type = evaluate_keyed_by(
-            worker_config["worker-type"],
-            worker_type,
-            {"level": level},
-        ).format(level=level, alias=worker_type)
-        return worker_config["provisioner"], worker_type
+    worker_config = evaluate_keyed_by(
+        {"by-worker-type": graph_config["workers"]["aliases"]},
+        "worker-types.yml",
+        {"worker-type": worker_type},
+    )
+    worker_type = evaluate_keyed_by(
+        worker_config["worker-type"],
+        worker_type,
+        {"level": level},
+    ).format(level=level, alias=worker_type)
+    return worker_config["provisioner"], worker_type
