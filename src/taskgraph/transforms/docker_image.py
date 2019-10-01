0a7e0e76f2fb6f7e141e8fac1d3a29e2c5f76ac1
Tom Prince <mozilla@hocat.ca>
2019-10-01 10:46:26 -0600

    Bug 1579756: Bump image_builder image; r=me

--- src/taskgraph/transforms/docker_image.py
+++ src/taskgraph/transforms/docker_image.py
@@ -26,8 +26,8 @@ DIGEST_RE = re.compile('^[0-9a-f]{64}$')
 
 IMAGE_BUILDER_BOOSTRAP_IMAGE = (
     'mozillareleases/taskgraph'
-    ':image_builder-19e0076d8238e9398db04369692def2237ee2e6019a9616cf80be2408bd9bb5e'
-    '@sha256:ced5cd043ec5111940ff2138324c177d2f89627a5e814b1d5f628aca16d4c0ff'
+    ':image_builder-3a4b3f0efe0403918de72c595eed804ab302fcaff9a0395a46933a926a467b3c'
+    '@sha256:d50b70d0bb379dc095a7381a185aae9228d753bd07536572a44011a8d5cadae7'
 )
 
 transforms = TransformSequence()
