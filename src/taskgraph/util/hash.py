d65afc8075ee1643dae9d5641f7d1f749a2e8774
Tom Prince <mozilla@hocat.ca>
2020-03-20 21:42:08 +0000

    Bug 1566234: Add a vesion of taskgraph.util.hash.hash_paths that doesn't depend on mozbuild r=Callek
    The mozilla-central version depends on mozbuild, which isn't available outside
    of mozilla-central. Implement a version that behaves the same using pathlib2
    instead.
    
    Differential Revision: https://phabricator.services.mozilla.com/D53863

--- src/taskgraph/util/hash.py
+++ src/taskgraph/util/hash.py
@@ -4,8 +4,10 @@
 
 from __future__ import absolute_import, print_function, unicode_literals
 import hashlib
+from pathlib2 import Path
 
 from taskgraph.util.memoize import memoize
+from taskgraph.util import path as mozpath
 
 
 @memoize
@@ -16,3 +18,36 @@ def hash_path(path):
     """
     with open(path) as fh:
         return hashlib.sha256(fh.read()).hexdigest()
+
+
+def _find_files(base_path):
+    for path in Path(base_path).rglob('*'):
+        if path.is_file():
+            yield str(path)
+
+
+def hash_paths(base_path, patterns):
+    """
+    Give a list of path patterns, return a digest of the contents of all
+    the corresponding files, similarly to git tree objects or mercurial
+    manifests.
+
+    Each file is hashed. The list of all hashes and file paths is then
+    itself hashed to produce the result.
+    """
+    h = hashlib.sha256()
+
+    found = set()
+    for pattern in patterns:
+        files = _find_files(base_path)
+        matches = [path for path in files if mozpath.match(path, pattern)]
+        if matches:
+            found.update(matches)
+        else:
+            raise Exception('%s did not match anything' % pattern)
+    for path in sorted(found):
+        h.update('{} {}\n'.format(
+            hash_path(mozpath.abspath(mozpath.join(base_path, path))),
+            mozpath.normsep(path)
+        ))
+    return h.hexdigest()
