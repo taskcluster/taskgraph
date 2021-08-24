6aa19eb0ae7d2188553da50126991282f820d2ad
Andrew Halberstadt <ahal@mozilla.com>
2021-08-24 15:42:40 +0000

    Bug 1714597 - Update minimum supported Python to 3.6, r=aki
    Differential Revision: https://phabricator.services.mozilla.com/D123128

--- taskcluster/docker/decision/system-setup.sh
+++ taskcluster/docker/decision/system-setup.sh
@@ -4,14 +4,12 @@ set -v -e
 
 test "$(whoami)" == 'root'
 
+# Python 2 is still needed for mercurial 4.7.2 (see install-mercurial.sh)
 apt-get update
 apt-get install -y --force-yes --no-install-recommends \
     ca-certificates \
     openssh-client \
     python \
-    python-pip \
-    python-setuptools \
-    python-wheel \
     python3 \
     python3-pip \
     python3-setuptools \
@@ -26,15 +24,13 @@ mkdir "$BUILD"
 
 tooltool_fetch() {
     cat >manifest.tt
-    python2.7 /setup/tooltool.py fetch
+    python3 /setup/tooltool.py fetch
     rm manifest.tt
 }
 
 cd $BUILD
 # shellcheck disable=SC1091
 . /setup/install-mercurial.sh
-
-pip install -r /setup/requirements.txt
 
 # Using pip3 directly results in a warning that a "very old" wrapper is being
 # used, and that support for this will be deprecated.
