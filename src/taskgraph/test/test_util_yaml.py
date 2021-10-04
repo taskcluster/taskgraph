fe66e45e44fbc8f670003f42ade59e070b0c3b21
Andrew Halberstadt <ahal@pm.me>
2021-10-04 18:41:30 +0000

    No Bug - Sync 'test_util_yaml.py' test from mozilla-central, r=taskgraph-reviewers,jmaher
    The 'util.yaml' module is the first thing that gecko taskgraph will drop and
    use from standalone taskgraph.  Syncing the test first to make sure we don't
    lose any test coverage in the process.
    
    Differential Revision: https://phabricator.services.mozilla.com/D127456

--- src/taskgraph/test/test_util_yaml.py
+++ src/taskgraph/test/test_util_yaml.py
@@ -4,20 +4,43 @@
 
 
 import unittest
+from textwrap import dedent
 
+from taskgraph.test.mockedopen import MockedOpen
 from taskgraph.util import yaml
-
-from .mockedopen import MockedOpen
-
-FOO_YML = """\
-prop:
-    - val1
-"""
 
 
 class TestYaml(unittest.TestCase):
     def test_load(self):
-        with MockedOpen({"/dir1/dir2/foo.yml": FOO_YML}):
+        with MockedOpen(
+            {
+                "/dir1/dir2/foo.yml": dedent(
+                    """\
+                    prop:
+                        - val1
+                    """
+                )
+            }
+        ):
+
             self.assertEqual(
                 yaml.load_yaml("/dir1/dir2", "foo.yml"), {"prop": ["val1"]}
+            )
+
+    def test_key_order(self):
+        with MockedOpen(
+            {
+                "/dir1/dir2/foo.yml": dedent(
+                    """\
+                    job:
+                        foo: 1
+                        bar: 2
+                        xyz: 3
+                    """
+                )
+            }
+        ):
+            self.assertEqual(
+                list(yaml.load_yaml("/dir1/dir2", "foo.yml")["job"].keys()),
+                ["foo", "bar", "xyz"],
             )
