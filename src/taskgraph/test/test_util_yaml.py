ef18b04a216acce51f6c5be41eba7caecdf52234
Andrew Halberstadt <ahal@pm.me>
2021-10-04 18:41:30 +0000

    No Bug - Convert 'test_util_yaml.py' to the pytest format, r=taskgraph-reviewers,jmaher
    This is now possible since it no longer needs to be synced with Gecko.
    
    Depends on D127456
    
    Differential Revision: https://phabricator.services.mozilla.com/D127457

--- src/taskgraph/test/test_util_yaml.py
+++ src/taskgraph/test/test_util_yaml.py
@@ -2,45 +2,42 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-
-import unittest
 from textwrap import dedent
 
 from taskgraph.test.mockedopen import MockedOpen
 from taskgraph.util import yaml
 
 
-class TestYaml(unittest.TestCase):
-    def test_load(self):
-        with MockedOpen(
-            {
-                "/dir1/dir2/foo.yml": dedent(
-                    """\
-                    prop:
-                        - val1
-                    """
-                )
-            }
-        ):
-
-            self.assertEqual(
-                yaml.load_yaml("/dir1/dir2", "foo.yml"), {"prop": ["val1"]}
+def test_load():
+    with MockedOpen(
+        {
+            "/dir1/dir2/foo.yml": dedent(
+                """\
+                prop:
+                    - val1
+                """
             )
+        }
+    ):
 
-    def test_key_order(self):
-        with MockedOpen(
-            {
-                "/dir1/dir2/foo.yml": dedent(
-                    """\
-                    job:
-                        foo: 1
-                        bar: 2
-                        xyz: 3
-                    """
-                )
-            }
-        ):
-            self.assertEqual(
-                list(yaml.load_yaml("/dir1/dir2", "foo.yml")["job"].keys()),
-                ["foo", "bar", "xyz"],
+        assert yaml.load_yaml("/dir1/dir2", "foo.yml") == {"prop": ["val1"]}
+
+
+def test_key_order():
+    with MockedOpen(
+        {
+            "/dir1/dir2/foo.yml": dedent(
+                """\
+                job:
+                    foo: 1
+                    bar: 2
+                    xyz: 3
+                """
             )
+        }
+    ):
+        assert list(yaml.load_yaml("/dir1/dir2", "foo.yml")["job"].keys()) == [
+            "foo",
+            "bar",
+            "xyz",
+        ]
