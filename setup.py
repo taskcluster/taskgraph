5ef226d8263ea974983d2a3e37f02afff5802a9f
Andrew Halberstadt <ahal@pm.me>
2022-01-10 18:28:58 +0000

    No Bug - Version bump to 1.1.1, r=releng-reviewers,jmaher
    Differential Revision: https://phabricator.services.mozilla.com/D135230

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("requirements/base.in", "r") as fp:
 
 setup(
     name="taskcluster-taskgraph",
-    version="1.1.0",
+    version="1.1.1",
     description="Build taskcluster taskgraphs",
     url="https://hg.mozilla.org/ci/taskgraph",
     packages=find_packages("src"),
