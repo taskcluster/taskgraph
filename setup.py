3da62d38d0a58142ae49a946b56a1c0f95db5ccd
Andrew Halberstadt <ahal@mozilla.com>
2022-05-25 10:07:48 -0400

    BREAKING CHANGE: drop support for Python 3.6
    Issue: #26

--- setup.py
+++ setup.py
@@ -15,7 +15,6 @@ setup(
         "Development Status :: 5 - Production/Stable",
         "Environment :: Console",
         "License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
