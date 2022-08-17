42a10dad420270e44e89a8f8f9f8f00283f02f76
Andrew Halberstadt <ahal@mozilla.com>
2022-08-17 17:04:01 -0400

    test: convert test_decision.py to the pytest format

--- test/test_decision.py
+++ test/test_decision.py
@@ -7,7 +7,6 @@ import json
 import os
 import shutil
 import tempfile
-import unittest
 from pathlib import Path
 
 import pytest
@@ -18,70 +17,66 @@ from taskgraph.util.yaml import load_yaml
 FAKE_GRAPH_CONFIG = {"product-dir": "browser", "taskgraph": {}}
 
 
-class TestDecision(unittest.TestCase):
-    def test_write_artifact_json(self):
-        data = [{"some": "data"}]
-        tmpdir = tempfile.mkdtemp()
-        try:
-            decision.ARTIFACTS_DIR = Path(tmpdir) / "artifacts"
-            decision.write_artifact("artifact.json", data)
-            with open(os.path.join(decision.ARTIFACTS_DIR, "artifact.json")) as f:
-                self.assertEqual(json.load(f), data)
-        finally:
-            if os.path.exists(tmpdir):
-                shutil.rmtree(tmpdir)
-            decision.ARTIFACTS_DIR = Path("artifacts")
-
-    def test_write_artifact_yml(self):
-        data = [{"some": "data"}]
-        tmpdir = tempfile.mkdtemp()
-        try:
-            decision.ARTIFACTS_DIR = Path(tmpdir) / "artifacts"
-            decision.write_artifact("artifact.yml", data)
-            self.assertEqual(load_yaml(decision.ARTIFACTS_DIR, "artifact.yml"), data)
-        finally:
-            if os.path.exists(tmpdir):
-                shutil.rmtree(tmpdir)
-            decision.ARTIFACTS_DIR = Path("artifacts")
+def test_write_artifact_json():
+    data = [{"some": "data"}]
+    tmpdir = tempfile.mkdtemp()
+    try:
+        decision.ARTIFACTS_DIR = Path(tmpdir) / "artifacts"
+        decision.write_artifact("artifact.json", data)
+        with open(os.path.join(decision.ARTIFACTS_DIR, "artifact.json")) as f:
+            assert json.load(f) == data
+    finally:
+        if os.path.exists(tmpdir):
+            shutil.rmtree(tmpdir)
+        decision.ARTIFACTS_DIR = Path("artifacts")
 
 
-class TestGetDecisionParameters(unittest.TestCase):
-    def setUp(self):
-        self.options = {
-            "base_repository": "https://hg.mozilla.org/mozilla-unified",
-            "head_repository": "https://hg.mozilla.org/mozilla-central",
-            "head_rev": "abcd",
-            "head_ref": "default",
-            "head_tag": "v0.0.1",
-            "project": "mozilla-central",
-            "pushlog_id": "143",
-            "pushdate": 1503691511,
-            "repository_type": "hg",
-            "owner": "nobody@mozilla.com",
-            "tasks_for": "hg-push",
-            "level": "3",
-        }
-        self.old_determine_more_accurate_base_rev = (
-            decision._determine_more_accurate_base_rev
-        )
-        decision._determine_more_accurate_base_rev = lambda *_, **__: "abcd"
+def test_write_artifact_yml():
+    data = [{"some": "data"}]
+    tmpdir = tempfile.mkdtemp()
+    try:
+        decision.ARTIFACTS_DIR = Path(tmpdir) / "artifacts"
+        decision.write_artifact("artifact.yml", data)
+        assert load_yaml(decision.ARTIFACTS_DIR, "artifact.yml") == data
+    finally:
+        if os.path.exists(tmpdir):
+            shutil.rmtree(tmpdir)
+        decision.ARTIFACTS_DIR = Path("artifacts")
 
-    def tearDown(self):
-        decision._determine_more_accurate_base_rev = (
-            self.old_determine_more_accurate_base_rev
-        )
 
-    def test_simple_options(self):
-        params = decision.get_decision_parameters(FAKE_GRAPH_CONFIG, self.options)
-        self.assertEqual(params["build_date"], 1503691511)
-        self.assertEqual(params["head_tag"], "v0.0.1")
-        self.assertEqual(params["pushlog_id"], "143")
-        self.assertEqual(params["moz_build_date"], "20170825200511")
+@pytest.fixture
+def options(monkeypatch):
+    monkeypatch.setattr(
+        decision, "_determine_more_accurate_base_rev", lambda *_, **__: "abcd"
+    )
+    return {
+        "base_repository": "https://hg.mozilla.org/mozilla-unified",
+        "head_repository": "https://hg.mozilla.org/mozilla-central",
+        "head_rev": "abcd",
+        "head_ref": "default",
+        "head_tag": "v0.0.1",
+        "project": "mozilla-central",
+        "pushlog_id": "143",
+        "pushdate": 1503691511,
+        "repository_type": "hg",
+        "owner": "nobody@mozilla.com",
+        "tasks_for": "hg-push",
+        "level": "3",
+    }
 
-    def test_no_email_owner(self):
-        self.options["owner"] = "ffxbld"
-        params = decision.get_decision_parameters(FAKE_GRAPH_CONFIG, self.options)
-        self.assertEqual(params["owner"], "ffxbld@noreply.mozilla.org")
+
+def test_simple_options(options):
+    params = decision.get_decision_parameters(FAKE_GRAPH_CONFIG, options)
+    assert params["build_date"] == 1503691511
+    assert params["head_tag"] == "v0.0.1"
+    assert params["pushlog_id"] == "143"
+    assert params["moz_build_date"] == "20170825200511"
+
+
+def test_no_email_owner(options):
+    options["owner"] = "ffxbld"
+    params = decision.get_decision_parameters(FAKE_GRAPH_CONFIG, options)
+    assert params["owner"] == "ffxbld@noreply.mozilla.org"
 
 
 @pytest.mark.parametrize(
@@ -93,9 +88,9 @@ class TestGetDecisionParameters(unittest.TestCase):
     ),
 )
 def test_determine_more_accurate_base_ref(
-    candidate_base_ref, base_rev, expected_base_ref
+    candidate_base_ref, base_rev, expected_base_ref, mocker
 ):
-    repo_mock = unittest.mock.MagicMock()
+    repo_mock = mocker.MagicMock()
     repo_mock.default_branch = "default-branch"
 
     assert (
@@ -121,9 +116,9 @@ def test_determine_more_accurate_base_ref(
     ),
 )
 def test_determine_more_accurate_base_rev(
-    common_rev, candidate_base_rev, expected_base_ref_or_rev, expected_base_rev
+    common_rev, candidate_base_rev, expected_base_ref_or_rev, expected_base_rev, mocker
 ):
-    repo_mock = unittest.mock.MagicMock()
+    repo_mock = mocker.MagicMock()
     repo_mock.find_latest_common_revision.return_value = common_rev
     repo_mock.does_revision_exist_locally = lambda rev: rev == "existing-rev"
 
