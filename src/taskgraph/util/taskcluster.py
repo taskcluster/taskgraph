ffa5311917b47965e2657302885ce74f7b18f02c
Andrew Halberstadt <ahal@mozilla.com>
2023-12-20 10:19:41 -0500

    revert: "Log the body of taskcluster errors"
    Refs: 4f364e66823cd4627175f3767c4d00b17b26fd11
    Issue: #400

--- src/taskgraph/util/taskcluster.py
+++ src/taskgraph/util/taskcluster.py
@@ -120,7 +120,7 @@ def _do_request(url, method=None, **kwargs):
     if response.status_code >= 400:
         # Consume content before raise_for_status, so that the connection can be
         # reused.
-        logger.warning("response body: %s", response.text)
+        response.content
     response.raise_for_status()
     return response
 
