2894da00d4fc95a66fd5cbe9f54f9bfb788c5fa6
Heitor Neiva <hneiva@mozilla.com>
2024-02-06 06:47:56 -0800

    feat: Add taskgraph to decision image + set base image to debian:12-slim (#427)

--- taskcluster/docker/decision/system-setup.sh
+++ taskcluster/docker/decision/system-setup.sh
@@ -25,9 +25,20 @@ cat >/etc/mercurial/hgrc.d/cacerts.rc <<EOF
 cacerts = ${CERT_PATH}
 EOF
 chmod 644 /etc/mercurial/hgrc.d/cacerts.rc
-chmod 644 /usr/local/mercurial/robustcheckout.py
 
+# Clone the repo in the decision image
 python3 -mpip install --break-system-packages -r /setup/requirements.txt
+python3 -mpip install --break-system-packages --no-deps /setup/taskgraph
+
+# Same files as include-run-task
+cp /setup/taskgraph/src/taskgraph/run-task/run-task /usr/local/bin/run-task
+chmod 744 /usr/local/bin/run-task
+cp /setup/taskgraph/src/taskgraph/run-task/fetch-content /usr/local/bin/fetch-content
+chmod 744 /usr/local/bin/fetch-content
+cp /setup/taskgraph/src/taskgraph/run-task/hgrc /etc/mercurial/hgrc.d/mozilla.rc
+chmod 644 /etc/mercurial/hgrc.d/mozilla.rc
+cp /setup/taskgraph/src/taskgraph/run-task/robustcheckout.py /opt/robustcheckout.py
+chmod 644 /opt/robustcheckout.py
 
 apt-get clean
 apt-get autoclean
