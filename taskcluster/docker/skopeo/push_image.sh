e48257f644b8d9f96345c4602db419c9f7af48e8
Andrew Halberstadt <ahal@pm.me>
2024-02-07 10:16:25 +0100

    ci: fix secret path in push_image.sh script

--- taskcluster/docker/skopeo/push_image.sh
+++ taskcluster/docker/skopeo/push_image.sh
@@ -11,7 +11,7 @@ test $VCS_HEAD_REPOSITORY
 test $VCS_HEAD_REV
 
 echo "=== Generating dockercfg ==="
-PASSWORD_URL=http://taskcluster/secrets/v1/secret/repo:github.com/mozilla-releng/balrog:dockerhub
+PASSWORD_URL="http://taskcluster/secrets/v1/secret/project/taskgraph/level-3/dockerhub"
 install -m 600 /dev/null $HOME/.dockercfg
 curl $PASSWORD_URL | jq '.secret.dockercfg' > $HOME/.dockercfg
 
