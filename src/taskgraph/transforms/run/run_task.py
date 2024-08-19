0137d23978d5eb80129c53e2cc16e160700e8a5d
Shiva Gupta <gshiva53@gmail.com>
2024-08-19 17:59:28 +0930

    fix: save vcs checkout dir path if exists

--- src/taskgraph/transforms/run/run_task.py
+++ src/taskgraph/transforms/run/run_task.py
@@ -82,7 +82,8 @@ def common_setup(config, task, taskdesc, command):
         for repo_config in repo_configs.values():
             checkout_path = path.join(vcs_path, repo_config.path)
             command.append(f"--{repo_config.prefix}-checkout={checkout_path}")
-            taskdesc["worker"]["env"]["VCS_CHECKOUT_DIR_PATH"] = checkout_path
+            if vcs_path is None or not vcs_path.strip():
+                taskdesc["worker"]["env"]["PRIMARY_REPO_PATH"] = checkout_path
 
         if run["sparse-profile"]:
             command.append(
