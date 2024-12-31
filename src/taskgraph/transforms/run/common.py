6473b75828403f7d4d468e496c1c3e78f4284f43
Andrew Halberstadt <ahal@mozilla.com>
2024-12-31 11:29:04 -0500

    feat: support setting repo wide default values for cache selection

--- src/taskgraph/transforms/run/common.py
+++ src/taskgraph/transforms/run/common.py
@@ -7,12 +7,12 @@ worker implementation they operate on, and take the same three parameters, for
 consistency.
 """
 
-import hashlib
 import json
 from typing import Any, Dict, List, Union
 
 from taskgraph.transforms.base import TransformConfig
 from taskgraph.util import path
+from taskgraph.util.caches import CACHES, get_checkout_dir
 from taskgraph.util.taskcluster import get_artifact_prefix
 
 
@@ -21,54 +21,6 @@ def get_vcsdir_name(os):
         return "src"
     else:
         return "vcs"
-
-
-def get_checkout_dir(task: Dict[str, Any]) -> str:
-    worker = task["worker"]
-    if worker["os"] == "windows":
-        return "build"
-    elif worker["implementation"] == "docker-worker":
-        return f"{task['run']['workdir']}/checkouts"
-    else:
-        return "checkouts"
-
-
-def get_checkout_cache_name(config: TransformConfig, task: Dict[str, Any]) -> str:
-    repo_configs = config.repo_configs
-    cache_name = "checkouts"
-
-    # Robust checkout does not clean up subrepositories, so ensure  that tasks
-    # that checkout different sets of paths have separate caches.
-    # See https://bugzilla.mozilla.org/show_bug.cgi?id=1631610
-    if len(repo_configs) > 1:
-        checkout_paths = {
-            "\t".join([repo_config.path, repo_config.prefix])
-            for repo_config in sorted(
-                repo_configs.values(), key=lambda repo_config: repo_config.path
-            )
-        }
-        checkout_paths_str = "\n".join(checkout_paths).encode("utf-8")
-        digest = hashlib.sha256(checkout_paths_str).hexdigest()
-        cache_name += f"-repos-{digest}"
-
-    # Sparse checkouts need their own cache because they can interfere
-    # with clients that aren't sparse aware.
-    if task["run"]["sparse-profile"]:
-        cache_name += "-sparse"
-
-    return cache_name
-
-
-CACHES = {
-    "cargo": {"env": "CARGO_HOME"},
-    "checkout": {
-        "cache_dir": get_checkout_dir,
-        "cache_name": get_checkout_cache_name,
-    },
-    "npm": {"env": "npm_config_cache"},
-    "pip": {"env": "PIP_CACHE_DIR"},
-    "uv": {"env": "UV_CACHE_DIR"},
-}
 
 
 def add_cache(task, taskdesc, name, mount_point, skip_untrusted=False):
@@ -216,8 +168,15 @@ def support_caches(
         workdir = workdir or "/builds/worker"
         base_cache_dir = path.join(workdir, base_cache_dir)
 
-    # Default to all caches enabled.
-    use_caches = run.get("use-caches", True)
+    use_caches = run.get("use-caches")
+    if use_caches is None:
+        # Use project default values for filtering caches, default to
+        # checkout cache if no selection is specified.
+        use_caches = (
+            config.graph_config.get("taskgraph", {})
+            .get("run", {})
+            .get("use-caches", True)
+        )
 
     for name, cache_cfg in CACHES.items():
         if not should_use_cache(name, use_caches, run["checkout"]):
