taskcluster/taskgraph

commit f85ff32682d469809fd49adf703bb6ca36ecbdc6

Let generic-worker verify run-task/fetch-content integrity (#666)
The decision task knows the contents of these scripts so it can pass
their checksums to downstream tasks.

src/taskgraph/transforms/run/run_task.py
@@ -6,7 +6,9 @@ Support for running tasks that are invoked via the `run-task` script.
 """
 
 import dataclasses
+import hashlib
 import os
+from pathlib import Path
 
 from voluptuous import Any, Optional, Required
 
@@ -24,6 +26,9 @@ EXEC_COMMANDS = {
     "bash": ["bash", "-cx"],
     "powershell": ["powershell.exe", "-ExecutionPolicy", "Bypass"],
 }
+
+RUN_TASK_PATH = Path(__file__).parent.parent.parent / "run-task" / "run-task"
+FETCH_CONTENT_PATH = Path(__file__).parent.parent.parent / "run-task" / "fetch-content"
 
 run_task_schema = Schema(
     {
@@ -169,10 +174,14 @@ def generic_worker_run_task(config, task, taskdesc):
     common_setup(config, task, taskdesc, command)
 
     worker.setdefault("mounts", [])
+    run_task_sha256 = hashlib.sha256(RUN_TASK_PATH.read_bytes()).hexdigest()
+    fetch_content_sha256 = hashlib.sha256(FETCH_CONTENT_PATH.read_bytes()).hexdigest()
     worker["mounts"].append(
         {
             "content": {
-                "url": script_url(config, "run-task"),
+                "taskId": {"task-reference": "<decision>"},
+                "artifact": "public/run-task",
+                "sha256": run_task_sha256,
             },
             "file": "./run-task",
         }
@@ -181,7 +190,9 @@ def generic_worker_run_task(config, task, taskdesc):
         worker["mounts"].append(
             {
                 "content": {
-                    "url": script_url(config, "fetch-content"),
+                    "taskId": {"task-reference": "<decision>"},
+                    "artifact": "public/fetch-content",
+                    "sha256": fetch_content_sha256,
                 },
                 "file": "./fetch-content",
             }

test/test_transforms_run_run_task.py
@@ -121,7 +121,9 @@ def assert_generic_worker(task):
                 {"cache-name": "checkouts", "directory": "build"},
                 {
                     "content": {
-                        "url": "https://tc-tests.localhost/api/queue/v1/task/<TASK_ID>/artifacts/public/run-task"
+                        "artifact": "public/run-task",
+                        "sha256": "581ca6876fac84fa2dd8e8c2c18677d790890e9675229fd34c912c937ae19fae",
+                        "taskId": {"task-reference": "<decision>"},
                     },
                     "file": "./run-task",
                 },

commit 45280db10e2b75e502898cf1a76736a71073be54

Fix run-task/fetch-content mounts definition
Fixes a regression from #666, which:
- used taskId instead of task-id in the task description
- failed to add sha256 to the schema

src/taskgraph/transforms/run/run_task.py
@@ -179,7 +179,7 @@ def generic_worker_run_task(config, task, taskdesc):
     worker["mounts"].append(
         {
             "content": {
-                "taskId": {"task-reference": "<decision>"},
+                "task-id": {"task-reference": "<decision>"},
                 "artifact": "public/run-task",
                 "sha256": run_task_sha256,
             },
@@ -190,7 +190,7 @@ def generic_worker_run_task(config, task, taskdesc):
         worker["mounts"].append(
             {
                 "content": {
-                    "taskId": {"task-reference": "<decision>"},
+                    "task-id": {"task-reference": "<decision>"},
                     "artifact": "public/fetch-content",
                     "sha256": fetch_content_sha256,
                 },

src/taskgraph/transforms/task.py
@@ -622,6 +622,8 @@ def build_docker_worker_payload(config, task, task_def):
                     # URL that supplies the content in response to an unauthenticated
                     # GET request.
                     Optional("url"): str,
+                    # SHA256 checksum of the content body
+                    Optional("sha256"): str,
                 },
                 # *** Either file or directory must be specified. ***
                 # If mounting a cache or read-only directory, the filesystem location of

test/test_transforms_run_run_task.py
@@ -8,7 +8,7 @@ from pprint import pprint
 import pytest
 
 from taskgraph.transforms.run import make_task_description
-from taskgraph.transforms.task import payload_builders
+from taskgraph.transforms.task import payload_builders, set_defaults
 from taskgraph.util.caches import CACHES
 from taskgraph.util.schema import Schema, validate_schema
 from taskgraph.util.templates import merge
@@ -70,6 +70,7 @@ def assert_docker_worker(task):
                 "-cx",
                 "echo hello world",
             ],
+            "docker-image": {"in-tree": "image"},
             "env": {
                 "CI_BASE_REPOSITORY": "http://hg.example.com",
                 "CI_HEAD_REF": "default",
@@ -87,6 +88,13 @@ def assert_docker_worker(task):
         },
         "worker-type": "t-linux",
     }
+    taskdesc = next(set_defaults({}, [task]))
+    taskdesc["worker"]["max-run-time"] = 0
+    validate_schema(
+        payload_builders[taskdesc["worker"]["implementation"]].schema,
+        taskdesc["worker"],
+        "validation error",
+    )
 
 
 def assert_generic_worker(task):
@@ -123,7 +131,7 @@ def assert_generic_worker(task):
                     "content": {
                         "artifact": "public/run-task",
                         "sha256": "581ca6876fac84fa2dd8e8c2c18677d790890e9675229fd34c912c937ae19fae",
-                        "taskId": {"task-reference": "<decision>"},
+                        "task-id": {"task-reference": "<decision>"},
                     },
                     "file": "./run-task",
                 },
@@ -132,6 +140,13 @@ def assert_generic_worker(task):
         },
         "worker-type": "b-win2012",
     }
+    taskdesc = next(set_defaults({}, [task]))
+    taskdesc["worker"]["max-run-time"] = 0
+    validate_schema(
+        payload_builders[taskdesc["worker"]["implementation"]].schema,
+        taskdesc["worker"],
+        "validation error",
+    )
 
 
 def assert_exec_with(task):
@@ -177,7 +192,7 @@ def assert_run_task_command_generic_worker(task):
     "task",
     (
         pytest.param(
-            {"worker": {"os": "linux"}},
+            {"worker": {"os": "linux", "docker-image": {"in-tree": "image"}}},
             id="docker_worker",
         ),
         pytest.param(