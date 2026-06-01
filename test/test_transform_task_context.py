6be42569eae99da1dbf3b050838bd73b0268128b
Ben Hearsum <ben@mozilla.com>
2026-05-31 21:06:11 -0400

    feat: allow resolving keys in task context
    This extends task-context to allow for calling `resolve_keyed_by` is a generic way. Combined with `task-context's` existing support for pulling context from parameters it removes the need for transforms for many usages of `resolve_keyed_by` (I counted at least a dozen or so in Gecko alone, and there's probably many more.)
    
    Mostly for simplicitly I chose to re-use `substitution-fields` as a source for what `resolve_keyed_by` should be called on. I could be convinced to maintain a separate list for that (although it might introduce repetition in certain cases). I'm honestly not sure which is preferable.

--- test/test_transform_task_context.py
+++ test/test_transform_task_context.py
@@ -1,5 +1,5 @@
 """
-Tests for the 'fetch' transforms.
+Tests for the 'task_context' transforms.
 """
 
 import os.path
@@ -13,6 +13,38 @@ from taskgraph.transforms import task_context
 from taskgraph.transforms.base import TransformConfig
 
 here = os.path.abspath(os.path.dirname(__file__))
+
+BASE_PARAMS = {
+    "base_repository": "http://hg.example.com",
+    "build_date": 0,
+    "build_number": 1,
+    "enable_always_target": True,
+    "head_repository": "http://hg.example.com",
+    "head_rev": "abcdef",
+    "head_ref": "default",
+    "level": "1",
+    "moz_build_date": 0,
+    "next_version": "1.0.1",
+    "owner": "some-owner",
+    "project": "some-project",
+    "pushlog_id": 1,
+    "repository_type": "hg",
+    "target_tasks_method": "test_method",
+    "tasks_for": "hg-push",
+    "try_mode": None,
+    "version": "1.0.0",
+}
+
+
+def make_config(graph_config, extra_params=None):
+    params_data = dict(BASE_PARAMS)
+    if extra_params:
+        params_data.update(extra_params)
+    params = FakeParameters(params_data)
+    return TransformConfig(
+        "test", str(here), {}, params, {}, graph_config, write_artifacts=False
+    )
+
 
 TASK_DEFAULTS = {
     "description": "fake description {object} {file} {param} {object_and_file} "
@@ -56,39 +88,135 @@ del NO_CONTEXT["task-context"]
     ),
 )
 def test_transforms(request, run_transform, graph_config, task, description):
-    params = FakeParameters(
+    config = make_config(
+        graph_config,
         {
             "param": "param",
             "object_and_param": "param-overrides-object",
             "file_and_param": "param-overrides-file",
             "object_file_and_param": "param-overrides-all",
             "default": "default",
-            "base_repository": "http://hg.example.com",
-            "build_date": 0,
-            "build_number": 1,
-            "enable_always_target": True,
-            "head_repository": "http://hg.example.com",
-            "head_rev": "abcdef",
-            "head_ref": "default",
-            "level": "1",
-            "moz_build_date": 0,
-            "next_version": "1.0.1",
-            "owner": "some-owner",
-            "project": "some-project",
-            "pushlog_id": 1,
-            "repository_type": "hg",
-            "target_tasks_method": "test_method",
-            "tasks_for": "hg-push",
-            "try_mode": None,
-            "version": "1.0.0",
         },
     )
-    transform_config = TransformConfig(
-        "test", str(here), {}, params, {}, graph_config, write_artifacts=False
-    )
 
-    task = run_transform(task_context.transforms, task, config=transform_config)[0]
+    task = run_transform(task_context.transforms, task, config=config)[0]
     print("Dumping task:")
     pprint(task, indent=2)
 
     assert task["description"] == description
+
+
+def test_resolve_keyed_by_from_parameters(run_transform, graph_config):
+    task = {
+        "name": "fake-task-name",
+        "description": {
+            "by-platform": {
+                "linux": "resolved-from-parameters",
+                "default": "default-value",
+            },
+        },
+        "task-context": {
+            "from-parameters": {"platform": "platform"},
+            "substitution-fields": ["description"],
+        },
+    }
+    config = make_config(graph_config, {"platform": "linux"})
+
+    task = run_transform(task_context.transforms, task, config=config)[0]
+    pprint(task, indent=2)
+
+    assert task["description"] == "resolved-from-parameters"
+
+
+def test_resolve_keyed_by_from_file(run_transform, graph_config):
+    task = {
+        "name": "fake-task-name",
+        "description": {
+            "by-file": {
+                "file": "resolved-from-file",
+                "default": "default-value",
+            },
+        },
+        "task-context": {
+            "from-file": f"{here}/data/task_context.yml",
+            "substitution-fields": ["description"],
+        },
+    }
+    config = make_config(graph_config)
+
+    task = run_transform(task_context.transforms, task, config=config)[0]
+    pprint(task, indent=2)
+
+    assert task["description"] == "resolved-from-file"
+
+
+def test_resolve_keyed_by_from_object(run_transform, graph_config):
+    task = {
+        "name": "fake-task-name",
+        "description": {
+            "by-flavor": {
+                "chocolate": "resolved-from-object",
+                "default": "default-value",
+            },
+        },
+        "task-context": {
+            "from-object": {"flavor": "chocolate"},
+            "substitution-fields": ["description"],
+        },
+    }
+    config = make_config(graph_config)
+
+    task = run_transform(task_context.transforms, task, config=config)[0]
+    pprint(task, indent=2)
+
+    assert task["description"] == "resolved-from-object"
+
+
+def test_resolve_keyed_by_missing_context_no_defer(run_transform, graph_config):
+    task = {
+        "name": "fake-task-name",
+        "description": {
+            "by-missing": {
+                "linux": "should-not-resolve",
+            },
+        },
+        "task-context": {
+            "substitution-fields": ["description"],
+        },
+    }
+    config = make_config(graph_config)
+
+    with pytest.raises(Exception):
+        run_transform(task_context.transforms, task, config=config)
+
+
+def test_resolve_keyed_by_missing_context_with_defer(run_transform, graph_config):
+    task = {
+        "name": "fake-task-name",
+        "description": {
+            "by-flavor": {
+                "chocolate": {
+                    "by-missing": {
+                        "linux": "should-not-resolve",
+                        "default": "also-should-not-resolve",
+                    },
+                },
+            },
+        },
+        "task-context": {
+            "from-object": {"flavor": "chocolate"},
+            "resolve-keyed-by-options": {"defer": ["missing"]},
+            "substitution-fields": ["description"],
+        },
+    }
+    config = make_config(graph_config)
+
+    task = run_transform(task_context.transforms, task, config=config)[0]
+    pprint(task, indent=2)
+
+    assert task["description"] == {
+        "by-missing": {
+            "linux": "should-not-resolve",
+            "default": "also-should-not-resolve",
+        },
+    }
