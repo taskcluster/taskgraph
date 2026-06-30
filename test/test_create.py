taskcluster/taskgraph

commit 76511bd120dfad9e67128c82c8a92fa1a72ed9da

fix: improve error handling on task creation failure
This prints the label of the task we failed to create for convenience,
as well as defers failing the Decision task until after we've attempted
all tasks. This way we'll see all the scope errors at once rather than
needing to fix them one at a time.

src/taskgraph/create.py
@@ -20,6 +20,18 @@ logger = logging.getLogger(__name__)
 testing = False
 
 
+class CreateTasksException(Exception):
+    """Exception raised when one or more tasks could not be created."""
+
+    def __init__(self, errors: dict[str, Exception]):
+        message = ""
+        for label, exc in errors.items():
+            message += f"\nERROR: Could not create '{label}':\n\n"
+            message += "\n".join(f"    {line}" for line in str(exc).splitlines()) + "\n"
+
+        super().__init__(message)
+
+
 def create_tasks(graph_config, taskgraph, label_to_taskid, params, decision_task_id):
     taskid_to_label = {t: l for l, t in label_to_taskid.items()}
 
@@ -50,6 +62,9 @@ def create_tasks(graph_config, taskgraph, label_to_taskid, params, decision_task
     session = get_session()
     with futures.ThreadPoolExecutor(concurrency) as e:
         fs = {}
+        fs_to_task = {}
+        skipped = set()
+        errors = {}
 
         # We can't submit a task until its dependencies have been submitted.
         # So our strategy is to walk the graph and submit tasks once all
@@ -57,11 +72,13 @@ def create_tasks(graph_config, taskgraph, label_to_taskid, params, decision_task
         tasklist = set(taskgraph.graph.visit_postorder())
         alltasks = tasklist.copy()
 
-        def schedule_tasks():
-            # bail out early if any futures have failed
-            if any(f.done() and f.exception() for f in fs.values()):
-                return
+        def handle_exception(fut):
+            if exc := fut.exception():
+                task_id, label = fs_to_task[fut]
+                skipped.add(task_id)
+                errors[label] = exc
 
+        def schedule_tasks():
             to_remove = set()
             new = set()
 
@@ -69,14 +86,24 @@ def create_tasks(graph_config, taskgraph, label_to_taskid, params, decision_task
                 fut = e.submit(create_task, session, task_id, label, task_def)
                 new.add(fut)
                 fs[task_id] = fut
+                fs_to_task[fut] = (task_id, label)
+                fut.add_done_callback(handle_exception)
 
             for task_id in tasklist:
                 task_def = taskgraph.tasks[task_id].task
-                # If we haven't finished submitting all our dependencies yet,
-                # come back to this later.
                 # Some dependencies aren't in our graph, so make sure to filter
                 # those out
                 deps = set(task_def.get("dependencies", [])) & alltasks
+
+                # If one of the dependencies didn't get created, then
+                # don't attempt to submit as it would fail.
+                if any(d in skipped for d in deps):
+                    skipped.add(task_id)
+                    to_remove.add(task_id)
+                    continue
+
+                # If we haven't finished submitting all our dependencies yet,
+                # come back to this later.
                 if any((d not in fs or not fs[d].done()) for d in deps):
                     continue
 
@@ -90,16 +117,18 @@ def create_tasks(graph_config, taskgraph, label_to_taskid, params, decision_task
                     submit(slugid(), taskid_to_label[task_id], task_def)
             tasklist.difference_update(to_remove)
 
-            # as each of those futures complete, try to schedule more tasks
+            # As each of those futures complete, try to schedule more tasks.
             for f in futures.as_completed(new):
                 schedule_tasks()
 
-        # start scheduling tasks and run until everything is scheduled
+        # Start scheduling tasks and run until everything is scheduled.
         schedule_tasks()
 
-        # check the result of each future, raising an exception if it failed
-        for f in futures.as_completed(fs.values()):
-            f.result()
+        # Wait for all futures to complete.
+        futures.wait(fs.values())
+
+        if errors:
+            raise CreateTasksException(errors)
 
 
 def create_task(session, task_id, label, task_def):

test/test_create.py
@@ -8,10 +8,10 @@ import unittest
 from unittest import mock
 
 import responses
-from taskcluster.exceptions import TaskclusterRestFailure
 
 from taskgraph import create
 from taskgraph.config import GraphConfig
+from taskgraph.create import CreateTasksException
 from taskgraph.graph import Graph
 from taskgraph.task import Task
 from taskgraph.taskgraph import TaskGraph
@@ -151,7 +151,7 @@ class TestCreate(unittest.TestCase):
         graph = Graph(nodes={"tid-a"}, edges=set())
         taskgraph = TaskGraph(tasks, graph)
 
-        with self.assertRaises(TaskclusterRestFailure):
+        with self.assertRaises(CreateTasksException):
             create.create_tasks(
                 GRAPH_CONFIG,
                 taskgraph,
@@ -159,3 +159,46 @@ class TestCreate(unittest.TestCase):
                 {"level": "4"},
                 decision_task_id="decisiontask",
             )
+
+    @responses.activate
+    @mock.patch.dict(
+        "os.environ",
+        {"TASKCLUSTER_ROOT_URL": "https://tc.example.com"},
+        clear=True,
+    )
+    def test_create_tasks_collects_multiple_errors(self):
+        "create_tasks collects all errors from multiple failing tasks"
+        mock_taskcluster_api(
+            error_status=409,
+            error_message={
+                "tid-a": "scope error for task a",
+                "tid-b": "scope error for task b",
+            },
+            error_task_ids={"tid-a", "tid-b"},
+        )
+
+        tasks = {
+            "tid-a": Task(
+                kind="test", label="a", attributes={}, task={"payload": "hello world"}
+            ),
+            "tid-b": Task(
+                kind="test", label="b", attributes={}, task={"payload": "hello world"}
+            ),
+        }
+        label_to_taskid = {"a": "tid-a", "b": "tid-b"}
+        graph = Graph(nodes={"tid-a", "tid-b"}, edges=set())
+        taskgraph = TaskGraph(tasks, graph)
+
+        with self.assertRaises(CreateTasksException) as cm:
+            create.create_tasks(
+                GRAPH_CONFIG,
+                taskgraph,
+                label_to_taskid,
+                {"level": "4"},
+                decision_task_id="decisiontask",
+            )
+
+        # Verify both errors are in the exception message
+        exception_message = str(cm.exception)
+        self.assertIn("Could not create 'a'", exception_message)
+        self.assertIn("Could not create 'b'", exception_message)

commit b5b6679f34f1b77e3f09cbb9d5efe65b60703de6

fix: ensure errors are not missed when creating tasks
I noticed this as an intermittent test failure in CI, which looks like a minor regression from https://github.com/taskcluster/taskgraph/pull/873:
```
[task 2026-06-29T11:51:43.891+00:00] ______________ TestCreate.test_create_tasks_fails_if_create_fails ______________
[task 2026-06-29T11:51:43.891+00:00] 
[task 2026-06-29T11:51:43.891+00:00] self = <test.test_create.TestCreate testMethod=test_create_tasks_fails_if_create_fails>
[task 2026-06-29T11:51:43.891+00:00] 
[task 2026-06-29T11:51:43.891+00:00]     @responses.activate
[task 2026-06-29T11:51:43.891+00:00]     @mock.patch.dict(
[task 2026-06-29T11:51:43.891+00:00]         "os.environ",
[task 2026-06-29T11:51:43.891+00:00]         {"TASKCLUSTER_ROOT_URL": "https://tc.example.com"},
[task 2026-06-29T11:51:43.891+00:00]         clear=True,
[task 2026-06-29T11:51:43.891+00:00]     )
[task 2026-06-29T11:51:43.891+00:00]     def test_create_tasks_fails_if_create_fails(self):
[task 2026-06-29T11:51:43.891+00:00]         "create_tasks fails if a single create_task call fails"
[task 2026-06-29T11:51:43.891+00:00]         mock_taskcluster_api(error_status=403, error_message="oh no!")
[task 2026-06-29T11:51:43.891+00:00]     
[task 2026-06-29T11:51:43.891+00:00]         tasks = {
[task 2026-06-29T11:51:43.891+00:00]             "tid-a": Task(
[task 2026-06-29T11:51:43.891+00:00]                 kind="test", label="a", attributes={}, task={"payload": "hello world"}
[task 2026-06-29T11:51:43.891+00:00]             ),
[task 2026-06-29T11:51:43.891+00:00]         }
[task 2026-06-29T11:51:43.891+00:00]         label_to_taskid = {"a": "tid-a"}
[task 2026-06-29T11:51:43.891+00:00]         graph = Graph(nodes={"tid-a"}, edges=set())
[task 2026-06-29T11:51:43.891+00:00]         taskgraph = TaskGraph(tasks, graph)
[task 2026-06-29T11:51:43.891+00:00]     
[task 2026-06-29T11:51:43.891+00:00] >       with self.assertRaises(CreateTasksException):

src/taskgraph/create.py
@@ -64,19 +64,12 @@ def create_tasks(graph_config, taskgraph, label_to_taskid, params, decision_task
         fs = {}
         fs_to_task = {}
         skipped = set()
-        errors = {}
 
         # We can't submit a task until its dependencies have been submitted.
         # So our strategy is to walk the graph and submit tasks once all
         # their dependencies have been submitted.
         tasklist = set(taskgraph.graph.visit_postorder())
         alltasks = tasklist.copy()
-
-        def handle_exception(fut):
-            if exc := fut.exception():
-                task_id, label = fs_to_task[fut]
-                skipped.add(task_id)
-                errors[label] = exc
 
         def schedule_tasks():
             to_remove = set()
@@ -87,7 +80,13 @@ def create_tasks(graph_config, taskgraph, label_to_taskid, params, decision_task
                 new.add(fut)
                 fs[task_id] = fut
                 fs_to_task[fut] = (task_id, label)
-                fut.add_done_callback(handle_exception)
+
+                def mark_failed_as_skipped(fut):
+                    if fut.exception():
+                        task_id, _ = fs_to_task[fut]
+                        skipped.add(task_id)
+
+                fut.add_done_callback(mark_failed_as_skipped)
 
             for task_id in tasklist:
                 task_def = taskgraph.tasks[task_id].task
@@ -126,6 +125,12 @@ def create_tasks(graph_config, taskgraph, label_to_taskid, params, decision_task
 
         # Wait for all futures to complete.
         futures.wait(fs.values())
+
+        # Collect errors.
+        errors = {}
+        for fut, (task_id, label) in fs_to_task.items():
+            if exc := fut.exception():
+                errors[label] = exc
 
         if errors:
             raise CreateTasksException(errors)

test/test_create.py
@@ -4,7 +4,9 @@
 
 import json
 import re
+import time
 import unittest
+from concurrent import futures
 from unittest import mock
 
 import responses
@@ -202,3 +204,43 @@ class TestCreate(unittest.TestCase):
         exception_message = str(cm.exception)
         self.assertIn("Could not create 'a'", exception_message)
         self.assertIn("Could not create 'b'", exception_message)
+
+    @responses.activate
+    @mock.patch.dict(
+        "os.environ",
+        {"TASKCLUSTER_ROOT_URL": "https://tc.example.com"},
+        clear=True,
+    )
+    def test_create_tasks_fails_if_done_callback_is_slow(self):
+        "create_tasks fails even if done-callbacks run after futures.wait() returns"
+        mock_taskcluster_api(error_status=403, error_message="oh no!")
+
+        tasks = {
+            "tid-a": Task(
+                kind="test", label="a", attributes={}, task={"payload": "hello world"}
+            ),
+        }
+        label_to_taskid = {"a": "tid-a"}
+        graph = Graph(nodes={"tid-a"}, edges=set())
+        taskgraph = TaskGraph(tasks, graph)
+
+        real_add_done_callback = futures.Future.add_done_callback
+
+        def slow_add_done_callback(self, fn):
+            def wrapper(fut):
+                time.sleep(0.1)
+                fn(fut)
+
+            return real_add_done_callback(self, wrapper)
+
+        with mock.patch.object(
+            futures.Future, "add_done_callback", slow_add_done_callback
+        ):
+            with self.assertRaises(CreateTasksException):
+                create.create_tasks(
+                    GRAPH_CONFIG,
+                    taskgraph,
+                    label_to_taskid,
+                    {"level": "4"},
+                    decision_task_id="decisiontask",
+                )